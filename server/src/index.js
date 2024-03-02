1c305173acf0effdc6de1ac19538a82f8c501455
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-03-03 04:55:00 +0600

    login route and jwt token

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
+const router = require("./router");
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
-
-app.get("/todos", (req, res) => {
-  res.send("hellow world");
-});
+app.use(router);
 
 app.listen(8080);
