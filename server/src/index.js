2851ab62512927ce0b02a1e7f49b13f628f523f9
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-03-03 03:35:22 +0600

    environtments setup and basic GET request

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("tiny"));
+
+app.get("/todos", (req, res) => {
+  res.send("hellow world");
+});
 
 app.listen(8080);
