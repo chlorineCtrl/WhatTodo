34e4ee3aeca86502a102562113caec53abceb602
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-03-03 20:36:58 +0600

    Update route added

--- server/src/router.js
+++ server/src/router.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const isLoggedIn = require("./middleware/isLoggedIn");
 
+const createTodoRoute = require("./routes/createTodoRoute");
+const readTodosRoute = require("./routes/readTodosRoute");
+const updateTodosRoute = require("./routes/updateTodosRoute");
+
 const router = express.Router();
 
+//auth
 router.post("/login", require("./routes/loginRoute"));
 
-router.post("/todos", isLoggedIn, require("./routes/createTodoRoute"));
-router.get("/todos", isLoggedIn, require("./routes/readTodosRoute"));
+//create
+router.post("/todos", isLoggedIn, createTodoRoute);
+
+//read
+router.get("/todos", isLoggedIn, readTodosRoute);
+
+//update
+router.put("/todos/:id", isLoggedIn, updateTodosRoute);
 
 module.exports = router;
